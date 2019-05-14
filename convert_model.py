a94ea01099d74daede81ec0a051da3d63b66a833
Konquer your Mind <breadbread1984@163.com>
2019-05-14 09:07:26 +0800

    Update convert_model.py

--- convert_model.py
+++ convert_model.py
@@ -12,9 +12,9 @@ def main():
     estimator = tf.estimator.Estimator(model_fn = action_model_fn, model_dir = "action_classifier_model");
     clip = tf.placeholder(dtype = tf.float32, shape = [None, 16, 112, 112, 3], name = "clip");
     serving_input_receiver_fn = tf.estimator.export.build_raw_serving_input_receiver_fn(features = {"features": clip});
-    estimator.export_saved_model('c3dfalling', serving_input_receiver_fn);
+    estimator.export_saved_model('c3d', serving_input_receiver_fn);
     # try to load the model once and test it
-    subdirs = [x for x in Path('c3dfalling').iterdir() if x.is_dir() and 'temp' not in str(x)];
+    subdirs = [x for x in Path('c3d').iterdir() if x.is_dir() and 'temp' not in str(x)];
     latest = str(sorted(subdirs)[-1]);
     predict_fn = predictor.from_saved_model(latest);
     # try to predict with the model once
